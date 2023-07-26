0690dc92fbf41aabdaa93e5d712241ceb1fffe5a
huythanh1324 <huythanh1324@gmail.com>
2023-07-26 14:35:18 +0700

    update navbar
    just part of it, not complete

--- gpt-3_js/src/components/navbar/Navbar.jsx
+++ gpt-3_js/src/components/navbar/Navbar.jsx
@@ -7,9 +7,20 @@ const Navbar = () => {
   return (
     <div className ="gpt3__navbar">
       <div className="gpt3__navbar-links">
-        <div className="gpt__navbar-links_logo">
-          <img source={logo} alt="logo"></img>
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo"></img>
         </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
+          <p><a href="#posibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
       </div>
     </div>
   )
