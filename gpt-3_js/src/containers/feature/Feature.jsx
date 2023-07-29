1465388a5a0b90fb5788d96e016fc00d5b1e0bba
huythanh1324 <huythanh1324@gmail.com>
2023-07-29 14:31:20 +0700

    update whatgpt3 section

--- gpt-3_js/src/containers/feature/Feature.jsx
+++ gpt-3_js/src/containers/feature/Feature.jsx
@@ -3,7 +3,20 @@ import './feature.css'
 
 const Feature = () => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__whatgpt3 section-margin">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
