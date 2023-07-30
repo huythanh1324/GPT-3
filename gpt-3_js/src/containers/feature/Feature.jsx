8a38f588d0bcc79fa296a3e646dd4c4c302bbad4
huythanh1324 <huythanh1324@gmail.com>
2023-07-30 11:49:53 +0700

    fix

--- gpt-3_js/src/containers/feature/Feature.jsx
+++ gpt-3_js/src/containers/feature/Feature.jsx
@@ -3,20 +3,7 @@ import './feature.css'
 
 const Feature = () => {
   return (
-    <div className="gpt3__whatgpt3 section-margin">
-      <div className="gpt3__whatgpt3-feature">
-        <Feature />
-      </div>
-      <div className="gpt3__whatgpt3-heading">
-        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
-        <p>Explore The Library</p>
-      </div>
-      <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
-      </div>
-    </div>
+    <div>Feature</div>
   )
 }
 
--- gpt-3_js/src/containers/whatGPT3/WhatGPT3.jsx
+++ gpt-3_js/src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,20 @@ import './whatgpt3.css'
 
 export const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className="gpt3__whatgpt3 section-margin">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 export default WhatGPT3
