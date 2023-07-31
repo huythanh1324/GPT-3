d82021fad305eed97c237955dd5be010ee448e8a
huythanh1324 <huythanh1324@gmail.com>
2023-07-31 16:40:01 +0700

    complete article + blog

--- gpt-3_js/src/components/article/Article.jsx
+++ gpt-3_js/src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({imageUrl, date, title}) => {
   return (
-    <div>article</div>
+    <div className="gpt3__blog-container_aritcle">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imageUrl} alt="" />
+      </div>
+      <div className="gpt3__blog-container_article-container">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read full article</p>
+      </div>
+    </div>
   )
 }
 
